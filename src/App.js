1f94a5ff0725538baa0e7665075b72df11ee2f7b
IkkyOdufade <ikmah_odufade@yahoo.com>
2019-07-02 14:04:43 +0100

    reactclock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Bukola from './components/bukola'
+// import Button from 'react-bootstrap/Button'
+
+import Clock from './components/clock'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+    
+      <div className="App-header">
+         {/* // <Button variant="flat" size="xl">click me</Button>
+  // <Button variant="primary">Primary</Button>
+  //   <Button variant="secondary">Secondary</Button>
+  //   <Button variant="success">Success</Button>
+  //   <Button variant="warning">Warning</Button>
+  //   <Button variant="danger">Danger</Button>
+  //   <Button variant="info">Info</Button>
+  //   <Button variant="light">Light</Button>
+  //   <Button variant="dark">Dark</Button>
+  //   <Button variant="link">Link</Button>
+       */}
+    <h1>  On the awesome server, I live...</h1>
 
-export default App;
+    <Clock></Clock>
+           <Bukola></Bukola>
+    </div>
+  
+    )}
+  
+  export default App;
+  
+  
---
+++ src/components/bukola.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Bukola() {
+  return (
+    
+      <h6>
+        I'm learning pretty code.
+      </h6>
+
+   
+
+  );
+}
+
+export default Bukola;
